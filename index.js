9a4c6eb5f9de373166744bacac1f70a058c3958d
Adriana Zestroiu <adriana.zestroiu@yahoo.com>
2016-09-17 20:27:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function getFirstSelector(selector){
+  var element=document.querySelector(selector);
+  return element;
+}
+function nestedTarget(){
+
+}
+function increaseRankBy(n){
+
+}
+function deepestChild(){
+
+}
